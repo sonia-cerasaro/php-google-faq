1260256ed91f4225850bccddb9b4f307b8cd8224
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-04-30 15:17:34 +0200

    laravel mix works

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Google FAQ</title>
+    <link rel="stylesheet" href="./dist/css/master.css">
+  </head>
+  <body>
+    <header>
+      <h1>Prova</h1>
+
+    </header>
+    <main>
+
+    </main>
+    <footer></footer>
+
+
+  </body>
+</html>
